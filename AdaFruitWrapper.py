3419757e592987455aa185014504fb0b9c5b9c0a
yapws87 <yapws87@gmail.com>
2023-12-02 00:21:25 +0900

    improve error

--- AdaFruitWrapper.py
+++ AdaFruitWrapper.py
@@ -7,16 +7,18 @@ ada_pw = os.getenv('ADAFRUIT_IO_PW')
 aio = Client(ada_id, ada_pw)
 
 
-class AdaFeed():
+class AdaFeed:
 
     def __init__(self, feed_name):
         try:
             print(f"AdaFeet __init__ {feed_name}")
             self.feed = aio.feeds(feed_name)
-        except RequestError:  # If feed doesn't exist, create it
-            print(f"AdaFeet __init__ CREATE {feed_name}")
-            self.feed = aio.create_feed(Feed(name=feed_name))
-    
+        except RequestError as e:  # If feed doesn't exist, create it
+            if e.response.status_code == 404: # Feed not found
+                print(f"AdaFeet __init__ CREATE {feed_name}")
+                self.feed = aio.create_feed(Feed(name=feed_name))
+            else:
+                raise
     def send_data(self,data):
         aio.send_data(self.feed.key, data)
 
